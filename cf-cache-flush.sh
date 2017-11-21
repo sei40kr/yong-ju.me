05fe46bf36803669430fc0accc02225c0597cb7a
Seong Yong-ju <sei40kr@gmail.com>
2017-11-22 00:27:33 +0900

    Clean up old files

--- cf-cache-flush.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
-  -H "X-Auth-Email: sei40kr@gmail.com" \
-  -H "X-Auth-Key: ${CF_API_KEY}" \
-  -H "Content-Type: application/json" \
-  -d '{"purge_everything":true}'
-
