d39543141dbcde49a0fc88d916005cfd672020fa
Seong Yong-ju <sei40kr@gmail.com>
2018-03-18 19:41:42 +0900

    Update timeline view
    Update timeline view for readability on mobile devices.

--- src/components/ExperienceItem/index.tsx
+++ src/components/ExperienceItem/index.tsx
@@ -17,13 +17,13 @@ const ExperienceItem = (props: IProps) => (
   <article className="row orig-experience-item">
     <div
       role="presentation"
-      className="pb-4 orig-experience-item__vertical-line"
+      className="d-none d-sm-flex pb-4 orig-experience-item__vertical-line"
     >
       <div role="presentation" className="d-block orig-experience-item__dot" />
     </div>
     <div
       role="presentation"
-      className="mb-4 ml-4 orig-experience-item__body-bubble"
+      className="d-none d-sm-flex mb-4 ml-4 orig-experience-item__body-bubble"
     />
 
     <section className="col mb-4 py-2 pl-4 orig-experience-item__body">
