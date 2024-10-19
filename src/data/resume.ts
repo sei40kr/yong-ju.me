75b56a147e48fa770632899c04afd20e00bdfa94
Seong Yong-ju <sei40kr@gmail.com>
2024-10-20 03:07:49 +0900

    fix: fetch resume via https

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -3,7 +3,7 @@ import { cache } from "@solidjs/router";
 export const getResume = cache(
   async () =>
     await ((
-      await fetch("http://jsonresume.yong-ju.me/resume.json")
+      await fetch("https://jsonresume.yong-ju.me/resume.json")
     ).json() as Promise<Resume>),
   "resume",
 );
